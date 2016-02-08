29c916a59cae4818276833f56dd9fc8783b641cc
Sergey Kozlov <mne@sergeykozlov.ru>
2016-02-08 16:57:44 +0300

    convertTimestamp

--- videme.js
+++ videme.js
@@ -643,7 +643,7 @@ target='_blank'>\
 						messageid='" + value.messageid + "' \
 						fromUserName='" + value.fromUserName + "' \
 						toUserName='" + value.toUserName + "' \
-						updatedAt='" + value.updatedAt + "' \
+						updatedAt='" + convertTimestamp(value.updatedAt) + "' \
 						subject='" + value.subject + "' \
 						message='" + value.message + "' \
 						href='https://vide.me/v?m=" + value.href + "&messageid=" + value.messageid + "' target='_blank'>\
@@ -664,28 +664,31 @@ target='_blank'>\
         return html;
     }
 
-    function paddingData(paddingData) {
-        $.each(paddingData, function (key, value) {
-            console.log("paddingData[key] ----->" + JSON.stringify(paddingData[key]));
-            //console.log("paddingData value ----->" + JSON.stringify(value));
-/*            paddingData[key] = {
-                'a': value.value.fromUserName,
-                'b': value.value.subject,
-                'c': value.value.message,
-                'd': value.value.updatedAt,
-                'img': value.value.file,
-                'href': value.value.file,
-                'fromUserName': value.value.fromUserName,
-                'subject': value.value.subject,
-                'message': value.value.message,
-                'updatedAt': value.value.updatedAt,
-                'file': value.value.file,
-                'messageid': value.id
-            };*/
-        });
-        //delete paddingData.results;
-        console.log("paddingData ----->" + JSON.stringify(paddingData));
-        return paddingData;
+    function convertTimestamp(timestamp) {
+        var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+            yyyy = d.getFullYear(),
+            mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+            dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+            hh = d.getHours(),
+            h = hh,
+            min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+            ampm = 'AM',
+            time;
+
+        if (hh > 12) {
+            h = hh - 12;
+            ampm = 'PM';
+        } else if (hh === 12) {
+            h = 12;
+            ampm = 'PM';
+        } else if (hh == 0) {
+            h = 12;
+        }
+
+        // ie: 2013-02-18, 8:35 AM
+        time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+
+        return time;
     }
 
     function parseFileInbox(parseFileInbox) {
